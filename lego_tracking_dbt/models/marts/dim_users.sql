8deeb0979879d7fab3946cac7091a4913327a781
mateuszbroja <mat.broja@gmail.com>
2025-02-02 16:54:01 +0100

    fixes dbt

--- lego_tracking_dbt/models/marts/dim_users.sql
+++ lego_tracking_dbt/models/marts/dim_users.sql
@@ -1,11 +1,15 @@
-select
-    domain_user_id,
-    min(event_timestamp) as first_seen_at,
-    max(event_timestamp) as last_seen_at,
-    count(distinct session_id) as total_sessions,
-    count(distinct date(event_timestamp)) as visited_days,
-    min(geo_city) as first_city,
-    min(geo_country) as first_country,
-    min(browser_language) as browser_language
-from {{ ref('stg_snowplow_events') }}
-group by 1
+with users as (
+   select
+       domain_user_id,
+       min(event_timestamp) as first_seen_at,
+       max(event_timestamp) as last_seen_at,
+       count(distinct session_id) as total_sessions,
+       count(distinct date(event_timestamp)) as visited_days,
+       min(geo_city) as first_city,
+       min(geo_country) as first_country,
+       min(browser_language) as browser_language
+   from {{ ref('stg_snowplow_events') }}
+   group by 1
+)
+
+select * from users
--- lego_tracking_dbt/models/marts/fct_sessions.sql
+++ lego_tracking_dbt/models/marts/fct_sessions.sql
@@ -1,14 +1,18 @@
-select
-    session_id,
-    domain_user_id,
-    min(event_timestamp) as session_start_at,
-    max(event_timestamp) as session_end_at,
-    count(*) as events_count,
-    count(distinct page_path) as unique_pages_viewed,
-    min(marketing_source) as first_marketing_source,
-    min(marketing_medium) as first_marketing_medium,
-    min(marketing_campaign) as first_marketing_campaign,
-    min(geo_city) as city,
-    min(geo_country) as country
-from {{ ref('stg_snowplow_events') }}
-group by 1, 2
+with sessions as (
+   select
+       session_id,
+       domain_user_id,
+       min(event_timestamp) as session_start_at,
+       max(event_timestamp) as session_end_at,
+       count(*) as events_count,
+       count(distinct page_path) as unique_pages_viewed,
+       min(marketing_source) as first_marketing_source,
+       min(marketing_medium) as first_marketing_medium,
+       min(marketing_campaign) as first_marketing_campaign,
+       min(geo_city) as city,
+       min(geo_country) as country
+   from {{ ref('stg_snowplow_events') }}
+   group by 1, 2
+)
+
+select * from sessions
